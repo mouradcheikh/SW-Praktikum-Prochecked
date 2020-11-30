16bf035bb9f5da5ea48df82d95a45d33bffb26aa
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2020-11-30 16:37:12 +0100

    UserView Design

--- Prochecked/frontend/src/Components/pages/UserView.js
+++ Prochecked/frontend/src/Components/pages/UserView.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, Typography, Paper } from '@material-ui/core';
-import { BankAPI } from '../api';
-import ContextErrorMessage from '../dialogs/ContextErrorMessage';
-import LoadingProgress from '../dialogs/LoadingProgress';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import './App.css';
 
 class UserView extends Component {
 
@@ -16,4 +14,70 @@ class UserView extends Component {
         loadingError: null,
       };
     }
+
+
+
+render () {
+    const classes = useStyles();
+
+	return(
+        <div>
+                <h1>Ich bin ein ...</h1>
+                <div>
+                <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                >
+                            Student
+                </Button>
+                </div>
+                <div>
+                <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    algin="center"
+                    className={classes.button}
+                
+                >
+                            Dozent
+                </Button>
+                </div>
+                <div>
+                <Button
+                     size="large"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                 >
+                            Admin
+                </Button>
+                </div>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    className={classes.submitButton}
+                >
+                        Bestätigen
+                </Button>
+      </div>
+    );
+  }
 }
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(2),
+    width: 170,
+    fontSize: 25,
+    padding: "15x 0"
+  },
+  submitButton: {
+    marginLeft: theme.spacing(50),
+    margin: theme.spacing(5)
+  },
+}));
+
+  export default UserView;
