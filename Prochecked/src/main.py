696448d5767e5459fc07329bd893c1788efdac50
Perry Dettke <perry.dettke@gmail.com>
2020-11-30 22:27:40 +0100

    Main.py in richtigen Ordner verschoben

--- Prochecked/src/main.py
+++ Prochecked/src/main.py
@@ -7,22 +7,22 @@ from flask_cors import CORS
 
 
 # Wir greifen natürlich auf unsere Applikationslogik inkl. BusinessObject-Klassen zurück
-from src.server.ProjectAdministration import ProjectAdministration
-from src.server.bo.Automat import Automat
-from src.server.bo.Grading import Grading
-from src.server.bo.Module import Module
-from src.server.bo.Participation import Participation
-from src.server.bo.Person import Person
-from src.server.bo.Project import Project
-from src.server.bo.ProjectState import ProjectState
-from src.server.bo.ProjectType import ProjectType
-from src.server.bo.Role import Role
-from src.server.bo.Semester import Semester
-from src.server.bo.Student import Student
+from server.ProjectAdministration import ProjectAdministration
+from server.bo.Automat import Automat
+from server.bo.Grading import Grading
+from server.bo.Module import Module
+from server.bo.Participation import Participation
+from server.bo.Person import Person
+from server.bo.Project import Project
+from server.bo.ProjectState import ProjectState
+from server.bo.ProjectType import ProjectType
+from server.bo.Role import Role
+from server.bo.Semester import Semester
+from server.bo.Student import Student
 
 
 # Außerdem nutzen wir einen selbstgeschriebenen Decorator, der die Authentifikation übernimmt
-from src.server.SecurityDecorator import secured
+from server.SecurityDecorator import secured
 
 """Hier wird Flask instanziert"""
 app = Flask(__name__)
