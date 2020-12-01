d4f560253e65deb8ad4ebc3f69888c7593edf6cf
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2020-12-01 16:28:25 +0100

    UserView korrigiert

--- Prochecked/frontend/src/Components/pages/UserView.js
+++ Prochecked/frontend/src/Components/pages/UserView.js
@@ -1,174 +1,67 @@
-// // import React, { Component } from 'react';
-// // import PropTypes from 'prop-types';
-// // import { withStyles, Typography, Paper } from '@material-ui/core';
-// // import { BankAPI } from '../api';
-// // import ContextErrorMessage from '../dialogs/ContextErrorMessage';
-// // import LoadingProgress from '../dialogs/LoadingProgress';
-import React from 'react';
-
- function UserView() {
-   return (
-   <div>
-    <h1>UserView</h1>
-   </div>
-   );
-
- }
-
- export default UserView;
-
-
-// // class UserView extends Component {
-
-// //     constructor(props) {
-// //       super(props);
-
-// //       // Init state
-// //       this.state = {
-// //         loadingInProgress: false,
-// //         loadingError: null,
-// //       };
-// //     }
-// // }
-
-import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+// import { Link as RouterLink } from 'react-router-dom'
 // import './App.css';
+import React, { Component } from 'react';
 
-// render () {
-//     const classes = useStyles();
+class UserView extends Component {
 
-// 	return(
-//         <div>
-//                 <h1>Ich bin ein ...</h1>
-//                 <div>
-//                 <Button
-//                     size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     className={classes.button}
-//                 >
-//                             Student
-//                 </Button>
-//                 </div>
-//                 <div>
-//                 <Button
-//                     size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     algin="center"
-//                     className={classes.button}
+    constructor(props) {
+      super(props);
+    }
+
+
+
+render () {
+  const { classes } = this.props;
+
+	return(
+        <div>
+                <h1>Ich bin ein ...</h1>
+                <div>
+                <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                >
+                            Student
+                </Button>
+                </div>
+                <div>
+                <Button
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    algin="center"
+                    className={classes.button}
                 
-//                 >
-//                             Dozent
-//                 </Button>
-//                 </div>
-//                 <div>
-//                 <Button
-//                      size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     className={classes.button}
-//                  >
-//                             Admin
-//                 </Button>
-//                 </div>
-//                 <Button
-//                     variant="contained"
-//                     color="secondary"
-//                     className={classes.submitButton}
-//                 >
-//                         Bestätigen
-//                 </Button>
-//       </div>
-//     );
-//   }
-// }
+                >
+                            Dozent
+                </Button>
+                </div>
+                <div>
+                <Button
+                     size="large"
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                 >
+                            Admin
+                </Button>
+                </div>
+      </div>
+    );
+  }
+}
 
-// const useStyles = makeStyles((theme) => ({
-//   button: {
-//     margin: theme.spacing(2),
-//     width: 170,
-//     fontSize: 25,
-//     padding: "15x 0"
-//   },
-//   submitButton: {
-//     marginLeft: theme.spacing(50),
-//     margin: theme.spacing(5)
-//   },
-// }));
+const styles = (theme) => ({
+  button: {
+    margin: theme.spacing(2),
+    width: 170,
+    fontSize: 25,
+    padding: "15x 0"
+  },
+})
 
-// //       // Init state
-// //       this.state = {
-// //         loadingInProgress: false,
-// //         loadingError: null,
-// //       };
-// //     }
-
-// function UserView()
-
-// render () {
-//     const { classes } = useStyles();
-
-// 	return(
-//         <div>
-//                 <h1>Ich bin ein ...</h1>
-//                 <div>
-//                 <Button
-//                     size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     className={classes.button}
-//                 >
-//                             Student
-//                 </Button>
-//                 </div>
-//                 <div>
-//                 <Button
-//                     size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     algin="center"
-//                     className={classes.button}
-                
-//                 >
-//                             Dozent
-//                 </Button>
-//                 </div>
-//                 <div>
-//                 <Button
-//                      size="large"
-//                     variant="contained"
-//                     color="primary"
-//                     className={classes.button}
-//                  >
-//                             Admin
-//                 </Button>
-//                 </div>
-//                 <Button
-//                     variant="contained"
-//                     color="secondary"
-//                     className={classes.submitButton}
-//                 >
-//                         Bestätigen
-//                 </Button>
-//       </div>
-//     );
-//   }
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   button: {
-//     margin: theme.spacing(2),
-//     width: 170,
-//     fontSize: 25,
-//     padding: "15x 0"
-//   },
-//   submitButton: {
-//     marginLeft: theme.spacing(50),
-//     margin: theme.spacing(5)
-//   },
-// }));
-
-//   export default makeStyles(useStyles)(UserView);
+  export default  withStyles(styles)(UserView);
