3277232d781d83ae509ba5f6e7639d023fb45f36
Mourad Cheikh <64478398+mouradcheikh@users.noreply.github.com>
2020-12-01 14:46:58 +0100

    Merge branch 'main' into mourad

--- Prochecked/frontend/src/Components/pages/UserView.js
+++ Prochecked/frontend/src/Components/pages/UserView.js
@@ -4,19 +4,18 @@
 // // import { BankAPI } from '../api';
 // // import ContextErrorMessage from '../dialogs/ContextErrorMessage';
 // // import LoadingProgress from '../dialogs/LoadingProgress';
+import React from 'react';
 
-// import React from 'react';
+ function UserView() {
+   return (
+   <div>
+    <h1>UserView</h1>
+   </div>
+   );
 
-// function UserView() {
-//   return (
-//   <div>
-//     <h1>UserView</h1>
-//   </div>
-//   );
+ }
 
-// }
-
-// export default UserView;
+ export default UserView;
 
 
 // // class UserView extends Component {
@@ -37,139 +36,139 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 // import './App.css';
 
-render () {
-    const classes = useStyles();
+// render () {
+//     const classes = useStyles();
 
-	return(
-        <div>
-                <h1>Ich bin ein ...</h1>
-                <div>
-                <Button
-                    size="large"
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                >
-                            Student
-                </Button>
-                </div>
-                <div>
-                <Button
-                    size="large"
-                    variant="contained"
-                    color="primary"
-                    algin="center"
-                    className={classes.button}
+// 	return(
+//         <div>
+//                 <h1>Ich bin ein ...</h1>
+//                 <div>
+//                 <Button
+//                     size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     className={classes.button}
+//                 >
+//                             Student
+//                 </Button>
+//                 </div>
+//                 <div>
+//                 <Button
+//                     size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     algin="center"
+//                     className={classes.button}
                 
-                >
-                            Dozent
-                </Button>
-                </div>
-                <div>
-                <Button
-                     size="large"
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                 >
-                            Admin
-                </Button>
-                </div>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    className={classes.submitButton}
-                >
-                        Bestätigen
-                </Button>
-      </div>
-    );
-  }
-}
+//                 >
+//                             Dozent
+//                 </Button>
+//                 </div>
+//                 <div>
+//                 <Button
+//                      size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     className={classes.button}
+//                  >
+//                             Admin
+//                 </Button>
+//                 </div>
+//                 <Button
+//                     variant="contained"
+//                     color="secondary"
+//                     className={classes.submitButton}
+//                 >
+//                         Bestätigen
+//                 </Button>
+//       </div>
+//     );
+//   }
+// }
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(2),
-    width: 170,
-    fontSize: 25,
-    padding: "15x 0"
-  },
-  submitButton: {
-    marginLeft: theme.spacing(50),
-    margin: theme.spacing(5)
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(2),
+//     width: 170,
+//     fontSize: 25,
+//     padding: "15x 0"
+//   },
+//   submitButton: {
+//     marginLeft: theme.spacing(50),
+//     margin: theme.spacing(5)
+//   },
+// }));
 
-//       // Init state
-//       this.state = {
-//         loadingInProgress: false,
-//         loadingError: null,
-//       };
-//     }
+// //       // Init state
+// //       this.state = {
+// //         loadingInProgress: false,
+// //         loadingError: null,
+// //       };
+// //     }
 
-function UserView()
+// function UserView()
 
-render () {
-    const { classes } = useStyles();
+// render () {
+//     const { classes } = useStyles();
 
-	return(
-        <div>
-                <h1>Ich bin ein ...</h1>
-                <div>
-                <Button
-                    size="large"
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                >
-                            Student
-                </Button>
-                </div>
-                <div>
-                <Button
-                    size="large"
-                    variant="contained"
-                    color="primary"
-                    algin="center"
-                    className={classes.button}
+// 	return(
+//         <div>
+//                 <h1>Ich bin ein ...</h1>
+//                 <div>
+//                 <Button
+//                     size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     className={classes.button}
+//                 >
+//                             Student
+//                 </Button>
+//                 </div>
+//                 <div>
+//                 <Button
+//                     size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     algin="center"
+//                     className={classes.button}
                 
-                >
-                            Dozent
-                </Button>
-                </div>
-                <div>
-                <Button
-                     size="large"
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                 >
-                            Admin
-                </Button>
-                </div>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    className={classes.submitButton}
-                >
-                        Bestätigen
-                </Button>
-      </div>
-    );
-  }
-}
+//                 >
+//                             Dozent
+//                 </Button>
+//                 </div>
+//                 <div>
+//                 <Button
+//                      size="large"
+//                     variant="contained"
+//                     color="primary"
+//                     className={classes.button}
+//                  >
+//                             Admin
+//                 </Button>
+//                 </div>
+//                 <Button
+//                     variant="contained"
+//                     color="secondary"
+//                     className={classes.submitButton}
+//                 >
+//                         Bestätigen
+//                 </Button>
+//       </div>
+//     );
+//   }
+// }
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(2),
-    width: 170,
-    fontSize: 25,
-    padding: "15x 0"
-  },
-  submitButton: {
-    marginLeft: theme.spacing(50),
-    margin: theme.spacing(5)
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(2),
+//     width: 170,
+//     fontSize: 25,
+//     padding: "15x 0"
+//   },
+//   submitButton: {
+//     marginLeft: theme.spacing(50),
+//     margin: theme.spacing(5)
+//   },
+// }));
 
-  export default makeStyles(useStyles)(UserView);
+//   export default makeStyles(useStyles)(UserView);
