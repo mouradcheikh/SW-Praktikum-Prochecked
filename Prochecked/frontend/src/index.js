ada145390ffb8b3b9f89f64caa2dd53c3c100ec7
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2020-12-03 16:07:01 +0100

    Änderung index.js

--- Prochecked/frontend/src/index.js
+++ Prochecked/frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import UserView from "./Components/UserView";
 
 ReactDOM.render(<App />, document.getElementById("root"));
