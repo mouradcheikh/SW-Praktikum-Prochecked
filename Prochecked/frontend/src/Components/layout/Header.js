734d2abb4fa27258dce1fda69444a8ed60626914
mariusff <mariusff@users.noreply.github.com>
2020-12-05 22:18:22 +0100

    update_header

--- Prochecked/frontend/src/Components/layout/Header.js
+++ Prochecked/frontend/src/Components/layout/Header.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import SubjectIcon from '@material-ui/icons/Subject';
 
 class Header extends React.Component {
     constructor (props) {
@@ -20,13 +20,13 @@ render () {
         <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <IconButton edge="start" className={classes.menuButton} color="gray" aria-label="menu">
+            <SubjectIcon/>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+              ProChecked - Hochschule der Medien
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit"></Button>
         </Toolbar>
       </AppBar>
     </div>
