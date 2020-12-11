18e5d350e3f00a5e20321cd875458c19ce50fe3b
KaiKustermann <kai.kuster@gmx.de>
2020-12-11 21:44:11 +0100

    importfehler behebung personfom

--- Prochecked/frontend/src/Components/dialogs/PersonForm.js
+++ Prochecked/frontend/src/Components/dialogs/PersonForm.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import { AppApi, PersonBO } from '../../AppApi';
+import AppAPI  from '../../AppApi/AppApi';
+import PersonBO from '../../AppApi/PersonBO'
 import ContextErrorMessage from './ContextErrorMessage';
 import LoadingProgress from './LoadingProgress';
 
