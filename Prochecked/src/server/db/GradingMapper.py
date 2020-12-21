0c6334acc90148a2ef16c03a7cf238e488b43430
Mourad Cheikh <64478398+mouradcheikh@users.noreply.github.com>
2020-12-21 23:30:06 +0100

    find_all Methode in GradingMapper

--- Prochecked/src/server/db/GradingMapper.py
+++ Prochecked/src/server/db/GradingMapper.py
@@ -2,13 +2,29 @@
 #-*- coding: utf-8 -*-
 
 from server.db.Mapper import Mapper
+from server.bo.Grading import Grading
 
 class GradingMapper(Mapper):
     def __init__(self):
-        pass
+        super().__init__()
 
-    def find_all(self, ):
-        pass
+    def find_all(self ):
+        """Auslesen aller Noten.
+
+        :return Eine Sammlung mit GRading-Objekten, die sämtliche Noten der Studierenden
+                repräsentieren.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT * from prochecked.grading"
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        result = Grading.from_tuples(tuples)
+
+        self._cnx.commit()
+        cursor.close()
+        return result
 
     def find_by_id(self, ):
         pass
