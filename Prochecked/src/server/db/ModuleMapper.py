dad0795158f642889536ed8e4d259328da3827bd
Mourad Cheikh <64478398+mouradcheikh@users.noreply.github.com>
2020-12-21 23:39:38 +0100

    find_all methode in ModuleMapper erstellt

--- Prochecked/src/server/db/ModuleMapper.py
+++ Prochecked/src/server/db/ModuleMapper.py
@@ -2,13 +2,32 @@
 #-*- coding: utf-8 -*-
 
 from server.db.Mapper import Mapper
+from server.db.Module import Module
 
 class ModuleMapper(Mapper):
     def __init__(self):
-        pass
+        super().__init__()
 
-    def find_all(self, ):
-        pass
+    def find_all(self):
+         """Auslesen aller Module.
+            :return Eine Sammlung mit Module-Objekten, die sämtliche Module repräsentieren."""
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from prochecked.module")
+        tuples = cursor.fetchall()
+
+        for (id, creation_date,name,edv_nr) in tuples:
+            modul = Module()
+            modul.set_id(id)
+            modul.set_creation_date(creation_date)
+            modul.set_name(name)
+            modul.set_edv_nr(edv_nr)
+            result.append(modul)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
 
     def find_by_id(self, ):
         pass
