9ce92462cf32f52dc82c72031f8f386bc93fe3db
mariusff <mariusff@users.noreply.github.com>
2021-01-07 18:38:13 +0100

    Person_erstellen_Presentation_Layer

--- Prochecked/frontend/src/Components/pages/AdminView/CreatePerson.js
+++ Prochecked/frontend/src/Components/pages/AdminView/CreatePerson.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
 
 class CreatePerson extends Component {
     constructor(props) {
@@ -10,13 +15,58 @@ class CreatePerson extends Component {
     }
     
     render() {
+        const { classes  } = this.props;
 
         return (
+        <div>
             <div>
-            <h1>Person erstellen</h1>
+                <h1>Eine neue Person erstellen</h1>
             </div>
+            
+            <div>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField id="outlined-basic" label="Name" variant="outlined" />
+                    <TextField id="outlined-basic" label="Google-ID" variant="outlined" />
+                    <TextField id="outlined-basic" label="E-Mail" variant="outlined" />
+                    <TextField id="outlined-basic" label="Rolle" variant="outlined" />
+                </form>
+            </div>
+           
+
+            <div>
+            <Button
+                variant="contained"
+                color="default"
+                size="large"
+                className={classes.button}
+                startIcon={<SaveIcon />}>                         
+                    Person anlegen
+            </Button>
+            </div>
+            
+            
+
+        
+        </div>
+            
+
+
         )
     }
 }
 
-export default (CreatePerson);
+const styles = theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+      button: {
+        margin: theme.spacing(1),
+      },
+    }, 
+  });
+
+
+
+export default withStyles(styles)(CreatePerson);
--- Prochecked/frontend/src/Components/pages/AdminView/CreateSemester.js
+++ Prochecked/frontend/src/Components/pages/AdminView/CreateSemester.js
@@ -173,6 +173,9 @@ const styles = theme => ({
       color: theme.palette.text.secondary,
     },
   });
+
+
+
  
   export default withStyles(styles)(CreateSemester); 
 
