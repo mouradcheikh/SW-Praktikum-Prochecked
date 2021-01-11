f659ca3e1ade8912d5b14b4e80a500bf718aedbd
MertcanCinar <74102810+MertcanCinar@users.noreply.github.com>
2021-01-11 13:20:44 +0100

    CreateRoles Datei angelegt

---
+++ Prochecked/frontend/src/Components/pages/AdminView/CreateRoles.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  TextField,
+  withStyles,
+  Button,
+  List,
+  ListItem,
+  Link,
+  Typography,
+  Input,
+  Grid,
+} from "@material-ui/core";
+import { AppApi } from "../../../AppApi";
+import Paper from "@material-ui/core/Paper";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
