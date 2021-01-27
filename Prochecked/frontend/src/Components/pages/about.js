37615d3ec2937f88877125f9d9e139fd655ef2ed
mariusff <mariusff@users.noreply.github.com>
2021-01-27 12:16:45 +0100

    about.js_namen_ergänzt

--- Prochecked/frontend/src/Components/pages/about.js
+++ Prochecked/frontend/src/Components/pages/about.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-//zeigt die About Seite
+//Zeigt die "About" Seite der ProChecked-App
 
 function About() {
 
@@ -23,11 +23,16 @@ function About() {
     <Paper elevation={0} className={classes.root}>
       <div className={classes.content}>
         <Typography variant='h6'>
-          ProjeChecked-Team09
+          ProChecked-Team09
         </Typography>
         <br />
         <Typography>
-          React Frontend written by <Link href='https://github.com/mariusff'>Marius Fechter</Link>
+          React single page application written by: <Link href='https://github.com/mariusff'>Marius Fechter</Link>
+                                                    <Link href='https://github.com/RobinRingelberg'>Robin Ringelberg</Link>
+                                                    <Link href='https://github.com/KaiKustermann'>Kai Kustermann</Link>
+                                                    <Link href='https://github.com/Perry-Dettke'>Perry Dettke</Link>
+                                                    <Link href='https://github.com/MertcanCinar'>Mertcan Cinar</Link>
+                                                    <Link href='https://github.com/mouradcheikh'>Mourad Cheikh</Link>
         </Typography>
         <br />
         <Typography variant='body2'>
